1bb35e37a2db8fbb46a8065a5a9fda48b48882e3
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-05-14 16:36:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estebanjojoa/bottonnavigation/ComprasActivity.java
@@ -0,0 +1,19 @@
+package com.example.estebanjojoa.bottonnavigation;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComprasActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compras);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/estebanjojoa/bottonnavigation/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.estebanjojoa.bottonnavigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.Item_shop:
+
+                    Intent intent = new Intent(MainActivity.this, ComprasActivity.class);
+                    startActivity(intent);
+
+                    return true;
+                case R.id.Item_buy:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.Item_fav:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+
+                case R.id.Item_user:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+}
