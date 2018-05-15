36aa069fc825bd95d6d561f21a138a32e1bbb712
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-05-14 22:42:24 -0500

    commit 1

--- app/src/main/java/com/example/estebanjojoa/bottonnavigation/LoginActivityActivity.java
+++ app/src/main/java/com/example/estebanjojoa/bottonnavigation/LoginActivityActivity.java
@@ -1,13 +1,25 @@
 package com.example.estebanjojoa.bottonnavigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivityActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        button = (Button) findViewById(R.id.Btn_Login);
+    }
+
+    public void Show_Login(View view){
+        Intent intent = new Intent(LoginActivityActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
