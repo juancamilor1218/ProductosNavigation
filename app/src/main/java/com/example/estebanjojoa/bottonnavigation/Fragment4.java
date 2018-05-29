bd903c8560735720e3e7280017125301985d1c82
“juan <juancamilo971218@gmail.com>
2018-05-29 14:24:18 -0500

    interfaz perfil

--- app/src/main/java/com/example/estebanjojoa/bottonnavigation/Fragment4.java
+++ app/src/main/java/com/example/estebanjojoa/bottonnavigation/Fragment4.java
@@ -23,7 +23,7 @@ public class Fragment4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment4, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
 }
