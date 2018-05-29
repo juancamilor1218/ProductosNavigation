35661d9867df904fff22985dc5ad91fc5add5a8d
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-05-28 19:39:58 -0500

    commit 1

--- app/src/main/java/com/example/estebanjojoa/bottonnavigation/MainActivity.java
+++ app/src/main/java/com/example/estebanjojoa/bottonnavigation/MainActivity.java
@@ -4,39 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView navigation;
+    BottomNavigationView botto_navigation;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.Item_shop:
-                    
-
-                    return true;
-                case R.id.Item_buy:
-
-                    return true;
-                case R.id.Item_fav:
-
-                    return true;
-
-                case R.id.Item_user:
-
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +22,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        botto_navigation = (BottomNavigationView) findViewById(R.id.botton_navigation);
+        botto_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Item_shop:
+                         Fragment1 fragment1 = new Fragment1();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frameLayout, fragment1)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+
+                        break;
+                    case R.id.Item_buy:
+                        Fragment2 fragment2 = new Fragment2();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frameLayout, fragment2)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+
+                        break;
+                    case R.id.Item_fav:
+                        Fragment3 fragment3 = new Fragment3();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frameLayout, fragment3)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+
+                        break;
+
+                    case R.id.Item_user:
+                        Fragment4 fragment4 = new Fragment4();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frameLayout, fragment4)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+
+                        break;
+                }
+                return true;
+            }
+
+        });
 
     }
 
